bf8252a9c77e8ec34e7f65a6426ad9c7529feae6
bbaudry <bruno.baudry@salt.ch>
2020-09-11 08:35:53 +0200

    Copied source\flow\salttv\_QuitAndRelaunchSaltTV.hx to source\flow\tv\app\_QuitAndRelaunchSaltTV.hx

---
+++ source/flow/tv/app/_QuitAndRelaunchSaltTV.hx
@@ -0,0 +1,18 @@
+package flow.salttv;
+
+import tstool.process.Action;
+
+/**
+ * ...
+ * @author bb
+ */
+class _QuitAndRelaunchSaltTV extends Action 
+{
+
+	override public function create()
+	{
+		this._nextProcesses = [new ProblemSolved()];
+		super.create();
+	}
+	
+}

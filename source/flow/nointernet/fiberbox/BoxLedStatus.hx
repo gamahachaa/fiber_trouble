6005e8c7ec66a05c665623307455c02a05c09fab
bbaudry <bruno.baudry@salt.ch>
2020-10-28 11:59:46 +0100

    new slide BoxLEdStatus

---
+++ source/flow/nointernet/fiberbox/BoxLedStatus.hx
@@ -0,0 +1,18 @@
+package flow.nointernet.fiberbox;
+
+import tstool.process.Triplet;
+
+/**
+ * ...
+ * @author bb
+ */
+class BoxLedStatus extends Triplet 
+{
+
+	public function new() 
+	{
+		super();
+		
+	}
+	
+}
---
+++ source/flow/tv/remote/exttv/_ClickOKOnRemote.hx
@@ -0,0 +1,18 @@
+package flow.tv.remote.exttv;
+
+import tstool.process.Action;
+
+/**
+ * ...
+ * @author bb
+ */
+class _ClickOKOnRemote extends Action 
+{
+
+	public function new() 
+	{
+		super();
+		
+	}
+	
+}
